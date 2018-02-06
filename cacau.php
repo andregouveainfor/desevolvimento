da540f3547888fadb8dd8f118630600ef33cbe0b
xavier1978 <1978mmx@gmail.com>
2018-02-06 19:37:22 -0200

    primeiro commit

---
+++ cacau.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+
+
+
+<div class="row">
+	 <div class="col-sm-4">
+	<img src="img/bolo-brownie.jpg" alt="Bolo de cacau" class="img-responsive">
+    </div> 
+     
+
+	 <div class="col-sm-5">
+	 <ul> 
+	<h3>Modo de preparo:</h3>
+	<h3>Bolo de cacau</h3>
+	
+Ingredientes: 
+<li>4 Ovos</li>
+<li>2  xíc. Farinha de Amêndoas</li>
+<li>1/2 xíc. de Farinha de Aveia (A Monama tem ‘sem glúten’)</li>
+<li>1/2 xíc. Açúcar de coco (ou demerara)</li>
+<li>6 csopa Cacau em pó</li>
+<li>1 cchá Bicarbonato de sódio</li>
+<li>1/2 cchá Fermento em pó </li>
+<li>1/2 cchá Sal</li>
+<li>1/2 xíc. Purê de abóbora (japonesa/cabotian) ou Banana nanica amassada (DICA: Com a banana fica gosto resisdual, mas com a abóbora não fica!)</li>
+<li>1/4 xíc. Mel (ou Melado)</li>
+<li>3 csopa Leite de amêndoas (ou de sua preferência)</li>
+<li>2 cchá de Essência de baunilha</li>
+<li>3 csopa Óleo de coco</li>
+</ul>
+
+<br>
+<div class="smallfont" style="margin-bottom: 2px;"> <input value="leia mais" style="margin: 0px; padding: 0px; width: 65px; font-size: 12px;" onclick="if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') { this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = ''; this.innerText = ''; this.value = 'fechar'; } else { this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none'; this.innerText = ''; this.value = 'leia mais'; }" type="button">
+ </div>
+ <div class="alt2">
+ <div style="display: none;">
+
+<ol>
+Passos:
+<li>Em uma tigela, misture todos os ingredientes secos. Reserve.</li>
+
+<li>Bater bem os ovos com um fuet</li>
+
+<li>Misture aos ovos à abóbora, mel, óleo de coco, baunilha e continuar batendo até ficar homogêneo. (Acho mais prático usar um mixer para misturar essa parte!)</li>
+
+<li>Misture o creme aos secos com uma colher/fuet até obter uma massa homogênea.</li>
+
+<li>Deseje a massa em uma forma redonda de 20cm untada com óleo de coco e farinha de aveia. Leve ao forno pré-aquecido em 180°, por aproximadamente, 30-40 minutos</li>
+</ol>	
+	</div>	
+</div>	
+</div>
+</div>	
+<?php include "footer.php" ?>
